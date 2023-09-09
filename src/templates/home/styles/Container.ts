d4c8b13558e23445cf56b3a640359993e443aa76
Matheus Silva <matheus_lsilva@hotmail.com>
2023-09-09 20:56:04 -0300

    fix: removing relative position and fix z-index

--- src/templates/home/styles/Container.ts
+++ src/templates/home/styles/Container.ts
@@ -2,10 +2,10 @@ import styled from 'styled-components';
 import { pixelToRem } from '../../../utils/pixelToRem';
 
 export const Container = styled.main`
-  position: relative;
   width: 100%;
   max-width: 1480px;
   height: 100%;
   margin: 0 auto;
   padding: 0 ${pixelToRem(102)} 0 ${pixelToRem(112)};
+  z-index: 1;
 `;
